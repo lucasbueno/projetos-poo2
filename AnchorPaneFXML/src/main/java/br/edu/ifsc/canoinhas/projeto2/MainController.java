6903918d6824c09f2528035290555711e1614f47
Lucas Bueno <lucasbuenolb@gmail.com>
2019-08-07 20:14:11 -0300

    modificações feitas em aula

--- AnchorPaneFXML/src/main/java/br/edu/ifsc/canoinhas/projeto2/MainController.java
+++ AnchorPaneFXML/src/main/java/br/edu/ifsc/canoinhas/projeto2/MainController.java
@@ -1,23 +1,62 @@
 package br.edu.ifsc.canoinhas.projeto2;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
-	
+
 	@FXML
 	Button btnPrimeiro;
+
 	@FXML
 	Button btnSegundo;
+
+	@FXML
+	Button btnTerceiro;
+
+	@FXML
+	Button btnQuarto;
+
+	@FXML
+	Button btnQuinto;
+
 	@FXML
 	Label lblConteudo;
-	
+
 	public void tratarPrimeiro() {
-		lblConteudo.setText("Primeiro");
+		tratarClique(btnPrimeiro);
 	}
 
 	public void tratarSegundo() {
-		lblConteudo.setText("Segundo");
+		tratarClique(btnSegundo);
+	}
+
+	public void tratarTerceiro() {
+		tratarClique(btnTerceiro);
+	}
+
+	public void tratarQuarto() {
+		tratarClique(btnQuarto);
+	}
+
+	public void tratarQuinto() {
+		tratarClique(btnQuinto);
+	}
+
+	private void tratarClique(Button botao) {
+		lblConteudo.setText(botao.getText());
+		resetarBotoes();
+		botao.setPadding(new Insets(20, 0, 20, 0));
+	}
+
+	public void resetarBotoes() {
+		Insets padding = new Insets(10, 0, 10, 0);
+		btnPrimeiro.setPadding(padding);
+		btnSegundo.setPadding(padding);
+		btnTerceiro.setPadding(padding);
+		btnQuarto.setPadding(padding);
+		btnQuinto.setPadding(padding);
 	}
 }
